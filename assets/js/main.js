7618addb3ac2224183682bea72f0681c62c4d13f
Vusal <vusald79@gmail.com>
2023-04-19 12:11:20 +0400

    header add

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// let label = document.querySelector("#search-label");
+
+// label.addEventListener('click',()=>{
+//     label.classList.add('active');
+// });
+
+// document.querySelector('body').addEventListener('click',()=>{
+//     // label.classList.remove('active');
+// })
