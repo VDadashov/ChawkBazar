347ed5500137a1f1a33693f2e9c92e6de897882a
Vusal <vusald79@gmail.com>
2023-04-21 19:07:44 +0400

    first-slide add and header fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,50 @@
-// let label = document.querySelector("#search-label");
+let slides = document.querySelectorAll('.slide');
+let btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
 
-// label.addEventListener('click',()=>{
-//     label.classList.add('active');
-// });
+let manualNav = function (manual) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
 
-// document.querySelector('body').addEventListener('click',()=>{
-//     // label.classList.remove('active');
-// })
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+    });
+});
+
+let repeat = function (activeClass) {
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+
+    let repeater = () => {
+        setTimeout(function () {
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active');
+            });
+
+            slides[i].classList.add('active');
+            btns[i].classList.add('active');
+            i++;
+
+            if (slides.length == i) {
+                i = 0;
+            }
+            if (i >= slides.length) {
+                return;
+            }
+            repeater();
+        }, 5000);
+    }
+    repeater();
+}
+repeat();
